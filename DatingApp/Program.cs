fd7c4d9b5f46f6474dc25dccea9318f1e521561e
jayeshmishra121c@gmail.com <jayeshmishra121c@gmail.com>
2022-07-21 00:59:27 +0530

    Adding angular in Client folderf

--- DatingApp/Program.cs
+++ DatingApp/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
@@ -24,6 +25,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:50242"));
 
 app.UseAuthorization();
 
